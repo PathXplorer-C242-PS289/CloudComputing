67a619257e05e2fdc7408f3ce89fa6ae35360281
fdhlputraa <fdhlputraa@gmail.com>
2024-12-02 14:58:41 +0700

    riasec test

--- api/routes/riasec.js
+++ api/routes/riasec.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const router = express.Router();
 const riasecController = require("../controllers/riasecController");
 
-router.post("/start-test", riasecController.startRiasecTest);
-router.get("/active-test", riasecController.getActiveTest);
-router.post("/submit-answer", riasecController.submitAnswer);
-router.post("/finish-test", riasecController.finishRiasecTest);
-router.get("/get-recommendations", riasecController.getRiasecRecommendation);
-router.get("/results", riasecController.getRiasecResultbyUserId);
+const router = express.Router();
+
+router.post("/save", riasecController.saveTestResult);
+router.get("/result/:testId", riasecController.getTestResult);
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authRoutes = require("./routes/auth");
 const profileRoutes = require("./routes/profile");
 const riasecRoutes = require("./routes/riasec");
+const webinarRoutes = require("./routes/webinar");
 const limiter = require("./middlewares/rateLimiter");
 const errorHandler = require("./middlewares/errorHandler");
 require("./config/db");
@@ -17,7 +18,9 @@ app.use(errorHandler);
 // Routes parsing
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
-app.use("/api/riasec", riasecRoutes);
+// app.use("/api/riasec", riasecRoutes);
+app.use("/api/tests", riasecRoutes);
+app.use("/api/webinars", webinarRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
