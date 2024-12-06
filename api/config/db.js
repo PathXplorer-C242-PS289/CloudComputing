4bbb1b4cefcd68dbb2af1caa101261611dac5423
ssugab <jeongiezen2@gmail.com>
2024-12-06 07:16:00 +0000

    fix variable name

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  socketPath: process.env.INSTANCE_CONNECTION_NAME, // socketPath: '/cloudsql/pathxplorer-442211:asia-southeast2:pathxplorer-db', //perubahan kode
+  socketPath: process.env.INSTANCE_CONNECTION_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS, 
   database: process.env.DB_NAME,
